f6b61283e44f12f315c23824f63235ed62c4ec5b
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2022-05-04 23:26:11 -0300

    correciones en el footer

--- site_bookstore/src/app.js
+++ site_bookstore/src/app.js
@@ -1,36 +1,40 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
+var app = express();
+
+/* ****Enrutadores**** */
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
 
 var app = express();
 
-// view engine setup
+/* ****view engine setup**** */
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+/* ****config de archivos estaticos**** */
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.resolve(__dirname,'..','public')));
 
+/* ****middlewares de rutas**** */
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 
 
-/* app.get("/", (req,res) => res.sendFile(path.resolve(__dirname, "views", "index.html")));
-app.get("/products", (req,res) => res.sendFile(path.resolve(__dirname, "views", "products.html")));
+/* app.get("/products", (req,res) => res.sendFile(path.resolve(__dirname, "views", "products.html")));
 app.get("/product-detail", (req,res) => res.sendFile(path.resolve(__dirname, "views", "productDetail.html")));
 app.get("/product-cart", (req,res) => res.sendFile(path.resolve(__dirname, "views", "productCart.html")));
 app.get("/register", (req,res) => res.sendFile(path.resolve(__dirname, "views", "register.html")));
-app.get("/login", (req,res) => res.sendFile(path.resolve(__dirname, "views", "login.html"))); */
+app.get("/login", (req,res) => res.sendFile(path.resolve(__dirname, "views", "login.html")));  */
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
