f1f0c118fb051b400e36283e5b2bd8dcbd2cc118
Belén Victorio <victoriobell@gmail.com>
2022-05-31 23:42:42 -0300

    agregando rutas process

--- site_bookstore/src/routes/users.js
+++ site_bookstore/src/routes/users.js
@@ -4,7 +4,9 @@ var router = express.Router();
 const {register,processRegister ,processLogin,login} = require('../controllers/userController');
 
 /* users */
-router.get('/register', register)
-router.get('/login', login)
+router.get('/register', register);
+router.post('/register', processRegister);
+router.get('/login', login);
+router.post('/login', processLogin);
 
 module.exports = router;
