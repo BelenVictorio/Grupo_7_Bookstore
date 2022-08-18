f40e08fcd0e620712c9db3fd5f33d787003123da
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2022-08-18 10:20:04 -0300

    add nav-bar products

--- site_bookstore/src/controllers/productController.js
+++ site_bookstore/src/controllers/productController.js
@@ -1,57 +1,66 @@
 const path = require('path');
 const fs = require('fs');
 const db = require('../database/models');
-const {Op} = require('sequelize');
+const { Op } = require('sequelize');
 
 
 module.exports = {
     detail: (req, res) => {
-        db.Product.findByPk(req.params.id,{
-        include:['images']})
-        .then(products => {
-             res.render('productos/productDetail', {
-                products
-            })
+        db.Product.findByPk(req.params.id, {
+            include: ['images']
         })
-        .catch(error => console.log(error))
+            .then(products => {
+                res.render('productos/productDetail', {
+                    products
+                })
+            })
+            .catch(error => console.log(error))
     },
     cart: (req, res) => {
         let products = db.Product.findAll({
-            include : ['images']
+            include: ['images']
         })
         let categories = db.Category.findAll()
 
-        Promise.all([products,categories])
-        .then(([products, categories]) => {
-            return res.render('productos/productCart', {
-                products,
-                categories
+        Promise.all([products, categories])
+            .then(([products, categories]) => {
+                return res.render('productos/productCart', {
+                    products,
+                    categories
+                })
             })
-        })
-        
-        .catch(error => console.log(error))
+
+            .catch(error => console.log(error))
     },
 
     products: async (req, res) => {
-        await db.Product.findAll({include: ['images']})
-			.then(products => {
-                return res.render('productos/products', {
-                    products
-                })
-			})
-			.catch(error => console.log(error))
+        try {
+            if (req.query.category) {
+                let products = await db.Product.findAll(
+                    { where: { category_id: req.query.category }, include: ['images'] })
+                return res.render('productos/products', { products })
+            }
+            else {
+                let products = await db.Product.findAll({ include: ['images'] })
+                return res.render('productos/products',
+                    { products })
+            }
+        }
+        catch (error) { console.log(error) }
     },
 
     search: (req, res) => {
-        
-        const {keyword} = req.query;
 
-        db.Product.findAll({ where: {[Op.or]: [{name: {[Op.substring]: keyword}},{description: {[Op.substring]: keyword}}]},
-            
-        }).then(results => {return res.render('result', {results,keyword,})
+        const { keyword } = req.query;
+
+        db.Product.findAll({
+            where: { [Op.or]: [{ name: { [Op.substring]: keyword } }, { description: { [Op.substring]: keyword } }] },
+
+        }).then(results => {
+            return res.render('result', { results, keyword, })
         }).catch(error => console.log(error))
 
     },
-    
-    
+
+
 }
